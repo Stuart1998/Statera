9c97086f92db5635bcb560224e0f30022eb0d0d2
Stuart1998 <robostuart@gmail.com>
2014-07-08 16:13:52 -0600

    More build bar stuff.

--- WIP/BalancedAnnihilation/ui/main/shared/js/build.js
+++ WIP/BalancedAnnihilation/ui/main/shared/js/build.js
@@ -61,7 +61,7 @@ function BuildHotkeyModel() {
             "/pa/units/land/fabrication_bot_adv/fabrication_bot_adv.json": ["bot", 0],
             "/pa/units/land/assault_bot_adv/assault_bot_adv.json": ["bot", 2],
             "/pa/units/land/bot_tactical_missile/bot_tactical_missile.json": ["bot", 3],
-            "/pa/units/land/bot_aa/bot_aa.json": ["bot", 4],
+            "/pa/units/land/bot_aa_adv/bot_aa_adv.json": ["bot", 4],
             "/pa/units/land/bot_sniper/bot_sniper.json": ["bot", 5],
             "/pa/units/land/fabrication_bot_combat_adv/fabrication_bot_combat_adv.json": ["bot", 6],
             "/pa/units/land/fabrication_bot/fabrication_bot.json": ["bot", 8],
